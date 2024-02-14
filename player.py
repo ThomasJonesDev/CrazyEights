0b6acba99bfbcb11c26bf112c662162dd641bb16
Thomas Jones <thomasjones952@gmail.com>
2024-02-14 23:54:10 +0000

    Created player class

--- player.py
+++ player.py
@@ -1,4 +1,10 @@
 class Player:
 
-    def __init__(self, player_hand):
-        self.player_hand = player_hand
+    def __init__(self):
+        self.player_hand = []
+
+    def get_cards(self, card):
+        self.player_hand.append(card)
+
+    def play_card(self, card):
+        return self.player_hand.pop(card)
