9f7b34ef7d713f5203cd0a6a10ff87b287da5737
Thomas Jones <thomasjones952@gmail.com>
2024-02-14 23:54:54 +0000

    Created deck class

---
+++ deck.py
@@ -0,0 +1,25 @@
+import random
+from suits import Suits
+from card import Card
+
+
+class Deck:
+
+    def __init__(self):
+        self.deck = self.create_deck(self)
+        self.shuffle_deck(self)
+
+    @staticmethod
+    def create_deck(self):
+        deck = []
+        for suit in Suits:
+            for value in range(1, 14):
+                deck.append(Card(suit, value))
+        return deck
+
+    @staticmethod
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+
+    def draw_card(self):
+        return self.deck.pop()
