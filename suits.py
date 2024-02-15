577373f3c172da078304c1f1f2c7bf02bc3fa00d
Thomas Jones <thomasjones952@gmail.com>
2024-02-15 01:15:29 +0000

    Changed Enum from int to string

--- suits.py
+++ suits.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class Suits(Enum):
-    CLUB = 0
-    DIAMONDS = 1
-    HEARTS = 2
-    SPADES = 3
+    CLUBS = 'clubs'
+    DIAMONDS = 'diamonds'
+    HEARTS = 'hearts'
+    SPADES = 'spades'
