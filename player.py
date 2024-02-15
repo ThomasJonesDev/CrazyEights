a5a2ec62ea705e087c45327748ec77e30fd99bd7
Thomas Jones <thomasjones952@gmail.com>
2024-02-15 18:09:18 +0000

    Renamed function to better describe its purpose.

--- player.py
+++ player.py
@@ -6,7 +6,7 @@ class Player:
     def get_player_hand(self):
         return self.player_hand
 
-    def get_card_from_deck(self, card):
+    def add_card_to_hand(self, card):
         self.player_hand.append(card)
 
     def play_card(self, card):
