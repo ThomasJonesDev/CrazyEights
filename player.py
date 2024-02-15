26ab28fa7cb025de3acb5675936a8e972f8cb4b7
Thomas Jones <thomasjones952@gmail.com>
2024-02-15 01:15:04 +0000

    Added a getter for the players hand list, and renamed get_cards -> get_card_from_deck

--- player.py
+++ player.py
@@ -3,7 +3,10 @@ class Player:
     def __init__(self):
         self.player_hand = []
 
-    def get_cards(self, card):
+    def get_player_hand(self):
+        return self.player_hand
+
+    def get_card_from_deck(self, card):
         self.player_hand.append(card)
 
     def play_card(self, card):
