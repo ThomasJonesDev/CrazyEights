57b5c1f41195cff577c38ff92e988105c2912594
NotTheRealTomJ <thomasjones952@gmail.com>
2024-02-19 23:15:14 +0000

    Misc Changes

--- twitchplays.py
+++ twitchplays.py
@@ -5,8 +5,9 @@ from gamerender import GameRenderer
 from player import Player
 from deck import Deck
 from pile import Pile
-from card import Card
 from gamestate import GameState
+from crowdsourcing import CrowdSourcing
+from crazyeights import CrazyEights
 
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
@@ -21,10 +22,12 @@ def game_loop():
     # Initialise the game
     pygame.init()
     # Initialise the game render
-    game_renderer = GameRenderer()
-    clock = pygame.time.Clock()
+    game_renderer: 'GameRenderer' = GameRenderer()
+    clock: 'Clock' = pygame.time.Clock()
     # Countdown timer
     countdown = Countdown()
+    # Connect to Twitch
+    crowdsourcing = CrowdSourcing()
     # Create players
     twitch_player = Player()
     ai_player = Player()
@@ -58,21 +61,24 @@ def game_loop():
         if game_state is GameState.TWITCH_PLAYING:
             # TODO Fix the flickering with the countdown
             # 30 Second countdown
-            if countdown.get_start_time() is None:
+            if not countdown.is_countdown_running():
                 countdown.start_countdown()
-            if countdown.get_countdown_in_seconds() <= 0:
+            if countdown.is_countdown_running() is True and countdown.get_countdown_in_seconds() <= 0:
                 countdown.stop_countdown()
                 # TODO take input and update game state
+                crowdsourced_answer = crowdsourcing.get_crowdsourced_answer()
+                selected_card = CrazyEights.get_select_card(crowdsourced_answer, twitch_player.get_player_hand())
+                if CrazyEights.is_valid_move(selected_card, pile.get_pile()[-1]):
+                    pile.add_to_pile(twitch_player.play_card(selected_card))
         elif game_state is GameState.AI_PLAYING:
             # TODO get input and update game state
             pass
 
         # Render TODO fix the flickering
-        game_renderer.render(deck.get_deck(),
-                             pile.get_pile(),
-                             twitch_player.get_player_hand(),
-                             ai_player.get_player_hand())
-        if countdown.get_countdown_in_seconds() >= 0:
+        game_renderer.render(
+            deck, pile, twitch_player, ai_player)
+        if countdown.is_countdown_running() is True and countdown.get_countdown_in_seconds() >= 0:
             game_renderer.render_time_remaining(countdown.get_countdown_in_seconds())
 
-        clock.tick(60)
+        # Set to 1 FPS to prevent flickering when the timer is updating
+        clock.tick(1)
