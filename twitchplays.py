a66181b1b0bb8e1bac952313c446efd19a1de47f
Thomas Jones <thomasjones952@gmail.com>
2024-02-20 15:48:19 +0000

    Added check for if player has any valid moves, and added win condition for twitch player. Also other misc changes.

--- twitchplays.py
+++ twitchplays.py
@@ -12,7 +12,7 @@ from crazyeights import CrazyEights
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
 GAME_TITLE = "Twitch Plays"
-FPS = 60
+FPS = 1
 BACKGROUND_COLOUR = [0, 255, 0]
 BACK_OF_CARD = pygame.image.load('images/Playing Cards/card-back1.png')
 NUMBER_OF_STARTING_CARDS = 8
@@ -23,11 +23,11 @@ def game_loop():
     pygame.init()
     # Initialise the game render
     game_renderer: 'GameRenderer' = GameRenderer()
-    clock: 'Clock' = pygame.time.Clock()
+    clock: 'pygame.time.Clock' = pygame.time.Clock()
     # Countdown timer
-    countdown = Countdown()
+    countdown: 'Countdown' = Countdown()
     # Connect to Twitch
-    crowdsourcing = CrowdSourcing()
+    crowdsourcing: 'CrowdSourcing' = CrowdSourcing()
     # Create players
     twitch_player = Player()
     ai_player = Player()
@@ -52,6 +52,12 @@ def game_loop():
     game_over = False
     while not game_over:
 
+        # Render the game
+        game_renderer.render(
+            deck, pile, twitch_player, ai_player)
+        if countdown.is_countdown_running() is True and countdown.get_countdown_in_seconds() >= 0:
+            game_renderer.render_time_remaining(countdown.get_countdown_in_seconds())
+
         # Process Mouse/Keyboard Input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -59,26 +65,34 @@ def game_loop():
 
         # Update Game
         if game_state is GameState.TWITCH_PLAYING:
-            # TODO Fix the flickering with the countdown
-            # 30 Second countdown
+
+            # Check if player has any valid moves, if not take card from deck
+            if CrazyEights.check_if_any_valid_moves(twitch_player.get_player_hand(), pile.get_top_card()) is False:
+                twitch_player.add_card_to_hand(deck.draw_card())
+                continue  # continue to skip rest of loop to prevent counter from starting
+
+            # 30-Second countdown
             if not countdown.is_countdown_running():
                 countdown.start_countdown()
             if countdown.is_countdown_running() is True and countdown.get_countdown_in_seconds() <= 0:
                 countdown.stop_countdown()
-                # TODO take input and update game state
+                # Get the input from Twitch
                 crowdsourced_answer = crowdsourcing.get_crowdsourced_answer()
                 selected_card = CrazyEights.get_select_card(crowdsourced_answer, twitch_player.get_player_hand())
-                if CrazyEights.is_valid_move(selected_card, pile.get_pile()[-1]):
+                # If there is a valid card played, then play card and switch to AI's go
+                if CrazyEights.is_valid_move(selected_card, pile.get_top_card()):
                     pile.add_to_pile(twitch_player.play_card(selected_card))
+
+                # Check win conditions for Twitch Player
+                if twitch_player.get_how_many_cards_player_has() == 0:
+                    # Send message saying congratulations and break game loop
+                    print('Twitch Player wins')  # TODO change this to a win message using render class
+                    break
         elif game_state is GameState.AI_PLAYING:
             # TODO get input and update game state
             pass
 
-        # Render TODO fix the flickering
-        game_renderer.render(
-            deck, pile, twitch_player, ai_player)
-        if countdown.is_countdown_running() is True and countdown.get_countdown_in_seconds() >= 0:
-            game_renderer.render_time_remaining(countdown.get_countdown_in_seconds())
+        # Set to 1 FPS to prevent flickering when the timer is updating, aswell as no reason to update more frequently
+        clock.tick(FPS)
 
-        # Set to 1 FPS to prevent flickering when the timer is updating
-        clock.tick(1)
+    # TODO implement game over screen with the winner
