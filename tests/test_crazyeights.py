8e0b3b8a8bb5d2e2d1496c76bab246e112aa5d7c
Thomas Jones <thomasjones952@gmail.com>
2024-02-20 15:44:59 +0000

    Created test function for is_valid_move function

--- tests/test_crazyeights.py
+++ tests/test_crazyeights.py
@@ -6,6 +6,12 @@ from suits import Suits
 
 
 class TestCrazyEights(TestCase):
+    def test_is_valid_move(self):
+        card1 = Card(1, Suits.CLUBS)
+        card2 = Card(13, Suits.CLUBS)
+        self.assertTrue(CrazyEights.is_valid_move(card1, card1))
+        self.assertFalse(CrazyEights.is_valid_move(card1, card2))
+
     def test_get_select_card(self):
         player = Player()
         card = Card(13, Suits.SPADES)
