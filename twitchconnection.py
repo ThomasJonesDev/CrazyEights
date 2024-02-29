7e71e40392931f3bdf824a0bb594278d2ee3f202
Thomas Jones <thomasjones952@gmail.com>
2024-02-29 23:42:45 +0000

    Updated main game loop

--- twitchconnection.py
+++ twitchconnection.py
@@ -1,9 +1,28 @@
 import asyncio
-import websockets
+import os
+import requests
+import socket
+from dotenv import load_dotenv
+
+ENV_FILE = ".env"
+TWITCH_OAUTH_LINK = "https://id.twitch.tv/oauth2/token"
+TWITCH_IRC_URI = "irc.chat.twitch.tv"
+TWITCH_IRC_PORT = 6667
+ENCODING_FORMAT = "utf-8"
 
 
 class TwitchConnection:
 
     def __init__(self):
-        client = websockets.client
-        pass
+        load_dotenv()
+        oauth = os.getenv("OAUTH")
+        # Connect the bot to the channel
+        irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        irc.connect((TWITCH_IRC_URI, TWITCH_IRC_PORT))
+
+        irc.send(f"PASS {oauth}".encode(ENCODING_FORMAT))
+        irc.send(f"NICK bot".encode(ENCODING_FORMAT))
+        
+
+
+TwitchConnection()
