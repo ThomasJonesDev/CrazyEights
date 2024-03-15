39c10611f4231de291f39b3a9bc6eabaedb9b59d
NotTheRealTomJ <thomasjones952@gmail.com>
2024-03-15 16:08:51 +0000

    fix: updated file path from Windows style to UNIX style

--- card.py
+++ card.py
@@ -1,39 +1,48 @@
-from filepath import FilePath
-from pygame import image
 from typing import TYPE_CHECKING
+
+from pygame import image
+
+from filepath import FilePath
 
 if TYPE_CHECKING:
     from pygame import Surface
+
     from suits import Suits
 
 
 class Card:
 
-    def __init__(self, card_value: int, card_suit: 'Suits') -> None:
+    def __init__(self, card_value: int, card_suit: "Suits") -> None:
         self._card_value: int = card_value
         self._card_suit: Suits = card_suit
-        image_file_path: str = FilePath.get_file_path('images\\Playing Cards\\')
+        image_file_path: str = FilePath.get_file_path("images/Playing Cards/")
         self._card_image: Surface = image.load(
-            image_file_path + 'card-' + self._card_suit.value + '-' + str(self._card_value) + '.png')
+            image_file_path
+            + "card-"
+            + self._card_suit.value
+            + "-"
+            + str(self._card_value)
+            + ".png"
+        )
 
     def get_card_value(self) -> int:
         return self._card_value
 
-    def get_card_suit(self) -> 'Suits':
+    def get_card_suit(self) -> "Suits":
         return self._card_suit
 
-    def get_card_image(self) -> 'Surface':
+    def get_card_image(self) -> "Surface":
         return self._card_image
 
     @staticmethod
-    def get_face_card_value(value: chr) -> int:
+    def get_face_card_value(value: str) -> int:
         value = value.upper()
-        if value == 'A':
+        if value == "A":
             return 1
-        elif value == 'J':
+        elif value == "J":
             return 11
-        elif value == 'Q':
+        elif value == "Q":
             return 12
-        elif value == 'K':
+        elif value == "K":
             return 13
         return int(value)
