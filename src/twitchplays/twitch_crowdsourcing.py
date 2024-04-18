c363f3c3c63a71b6660e1fcac2c37e0c40e7f17f
NotTheRealTomJ <thomasjones952@gmail.com>
2024-04-18 03:29:57 +0100

    now returns list of answers in order of popularity

--- src/twitchplays/twitch_crowdsourcing.py
+++ src/twitchplays/twitch_crowdsourcing.py
@@ -37,63 +37,74 @@ class TwitchCrowdsourcing:
         self.twitch_connection.send_to_chat(START_SUBMITTING_ANSWERS_MESSAGE)
         self.twitch_connection.add_logging_checkpoint()
 
-    def get_submitted_answers(self) -> tuple[int, str]:
+    def get_submitted_answers(self) -> list[tuple[int, str]]:
         """
         Tells TwitchConnection to stop recording messages from chat
         Calulates which card was selected the most and returns it as a tuple
         """
         self.twitch_connection.send_to_chat(STOP_SUBMITTING_ANSWER_MESSAGE)
         messages: list[str] = self.twitch_connection.get_messages_since_checkpoint()
-        parsed_input: dict[str, str] = self.parse_input(messages)
-        filtered_input: dict[str, str] = self.filter_answers(parsed_input)
-        mode_move: str = self.get_the_mode_move(filtered_input)
-        return self.convert_mode_move_to_tuple(mode_move)
-
-    @staticmethod
-    def convert_mode_move_to_tuple(card: str) -> tuple[int, str]:
-        """
-        Takes the card represented as a string e.g. "10H" and returns it as a tuple (10, "H")
-        """
-        if len(card) == 3:
-            return (10, card[2])
-        return (int(card[0]), card[1])
-
-    @staticmethod
-    def get_the_mode_move(players_moves: dict[str, str]) -> str:
-        """
-        Gets the move that was choosen the most (the mode)
-        """
-        moves_counter: dict[str, int] = (
-            TwitchCrowdsourcing.create_move_counter_dictionary()
+        parsed_input: dict[str, str] = TwitchCrowdsourcing._parse_input(messages)
+        filtered_input: dict[str, str] = TwitchCrowdsourcing._filter_answers(
+            parsed_input
         )
-
-        for move in players_moves.values():
-            moves_counter[move] = moves_counter[move] + 1
-
-        key_of_max_value: str = ""
-        max_value: int = 0
-
-        for key, value in moves_counter.items():
-            if value > max_value:
-                key_of_max_value = key
-                max_value = value
-
-        return key_of_max_value
+        orderd_moves: list[str] = TwitchCrowdsourcing._get_list_of_choosen_cards(
+            filtered_input
+        )
+        return TwitchCrowdsourcing._convert_card_str_to_tuple(orderd_moves)
 
     @staticmethod
-    def create_move_counter_dictionary() -> dict[str, int]:
+    def _convert_card_str_to_tuple(card_list: list[str]) -> list[tuple[int, str]]:
         """
-        Creates a directory that contains all 52 cards in a deck, as the key
-        set the value to 0 for all keys
+        ...
         """
-        move_counter: dict[str, int] = {}
+        tuple_list: list[tuple[int, str]] = []
+        for card in card_list:
+            if len(card) == 3:
+                tuple_list.append((10, card[2]))
+            else:
+                tuple_list.append((int(card[0]), card[1]))
+
+        return tuple_list
+
+    @staticmethod
+    def _get_list_of_choosen_cards(chat_moves: dict[str, str]) -> list[str]:
+        """
+        ...
+        """
+
+        card_tally: dict[str, int] = TwitchCrowdsourcing._create_card_tally()
+
+        # Get a list of all submitted cards
+        for _, chat_val in chat_moves.items():
+            for tally_key, _ in card_tally.items():
+                if chat_val == tally_key:
+                    card_tally[tally_key] += 1
+                    break
+
+        # Remove cards with value of 0
+        temp_dict: dict[str, int] = {}
+        for key, value in card_tally.items():
+            if value > 0:
+                temp_dict[key] = value
+
+        # Count how many times a card is selected
+        sorted_list: list[str] = sorted(temp_dict, key=lambda x: x[1], reverse=True)
+        return sorted_list
+
+    @staticmethod
+    def _create_card_tally() -> dict[str, int]:
+        """
+        Creates a directory that contains all 52 cards in a deck, move, num_of_votes
+        """
+        tally: dict[str, int] = {}
         for suit in SUITS:
             for value in VALUES:
-                move_counter[value + suit] = 0
-        return move_counter
+                tally[value + suit] = 0
+        return tally
 
     @staticmethod
-    def parse_input(messages: list[str]) -> dict[str, str]:
+    def _parse_input(messages: list[str]) -> dict[str, str]:
         """
         Returns a dictionary that contains username, message
         e.g.
@@ -111,7 +122,7 @@ class TwitchCrowdsourcing:
         return parsed_input
 
     @staticmethod
-    def filter_answers(dictionary: dict[str, str]) -> dict[str, str]:
+    def _filter_answers(dictionary: dict[str, str]) -> dict[str, str]:
         """
         Takes a dictionary and removes all the entries which dont have a valid value
         """
--- tests/test_twitch_crowdsourcing.py
+++ tests/test_twitch_crowdsourcing.py
@@ -4,26 +4,84 @@ from unittest.case import TestCase
 from src.twitchplays.twitch_crowdsourcing import TwitchCrowdsourcing
 from src.twitchplays.twitchconnection import TwitchConnection
 
+card_dict: dict[str, int] = {
+    "AS": 0,
+    "2S": 0,
+    "3S": 0,
+    "4S": 0,
+    "5S": 0,
+    "6S": 0,
+    "7S": 0,
+    "8S": 0,
+    "9S": 0,
+    "10S": 0,
+    "JS": 0,
+    "QS": 0,
+    "KS": 0,
+    "AC": 0,
+    "2C": 0,
+    "3C": 0,
+    "4C": 0,
+    "5C": 0,
+    "6C": 0,
+    "7C": 0,
+    "8C": 0,
+    "9C": 0,
+    "10C": 0,
+    "JC": 0,
+    "QC": 0,
+    "KC": 0,
+    "AH": 0,
+    "2H": 0,
+    "3H": 0,
+    "4H": 0,
+    "5H": 0,
+    "6H": 0,
+    "7H": 0,
+    "8H": 0,
+    "9H": 0,
+    "10H": 0,
+    "JH": 0,
+    "QH": 0,
+    "KH": 0,
+    "AD": 0,
+    "2D": 0,
+    "3D": 0,
+    "4D": 0,
+    "5D": 0,
+    "6D": 0,
+    "7D": 0,
+    "8D": 0,
+    "9D": 0,
+    "10D": 0,
+    "JD": 0,
+    "QD": 0,
+    "KD": 0,
+}
+
 
 class TestTwitchCrowdsourcing(unittest.TestCase):
 
-    def test_convert_mode_move_to_tuple(self):
-        self.assertEqual(
-            (10, "H"), TwitchCrowdsourcing.convert_mode_move_to_tuple("10H")
-        )
-        self.assertEqual((1, "C"), TwitchCrowdsourcing.convert_mode_move_to_tuple("1C"))
+    def test_convert_card_str_to_tuple(self):
+        input: list[str] = ["10H", "4C"]
+        output: list[tuple[int, str]] = [(10, "H"), (4, "C")]
+        self.assertEqual(output, TwitchCrowdsourcing._convert_card_str_to_tuple(input))
 
-    def test_get_the_mode_move(self):
-        dictionary: dict[str, str] = {
+    def test_get_list_of_choosen_card(self):
+        input_dict: dict[str, str] = {
             "player1": "AS",
             "player2": "AC",
             "player3": "AS",
             "player4": "10S",
         }
 
-        self.assertEqual("AS", TwitchCrowdsourcing.get_the_mode_move(dictionary))
+        output_list: list[str] = ["AS", "AC", "10S"]
 
-    def test_create_move_counter_dictionary(self):
+        self.assertEqual(
+            output_list, TwitchCrowdsourcing._get_list_of_choosen_cards(input_dict)
+        )
+
+    def test_create_card_tally(self):
         dictionary: dict[str, int] = {
             "AS": 0,
             "2S": 0,
@@ -78,13 +136,14 @@ class TestTwitchCrowdsourcing(unittest.TestCase):
             "QD": 0,
             "KD": 0,
         }
-        self.assertEqual(
-            dictionary, TwitchCrowdsourcing.create_move_counter_dictionary()
-        )
+
+        self.assertEqual(dictionary, TwitchCrowdsourcing._create_card_tally())
 
     def test_parse_input(self):
         input: list[str] = [":foo!foo@foo.tmi.twitch.tv PRIVMSG #bar :bleedPurple"]
-        self.assertEqual({"foo": "bleedPurple"}, TwitchCrowdsourcing.parse_input(input))
+        self.assertEqual(
+            {"foo": "bleedPurple"}, TwitchCrowdsourcing._parse_input(input)
+        )
 
     def test_filter_answers(self):
         dictionary = {
@@ -94,7 +153,7 @@ class TestTwitchCrowdsourcing(unittest.TestCase):
             "player4": "fjasdlgjklgj",
         }
         self.assertEqual(
-            {"player3": "7S"}, TwitchCrowdsourcing.filter_answers(dictionary)
+            {"player3": "7S"}, TwitchCrowdsourcing._filter_answers(dictionary)
         )
 
 
